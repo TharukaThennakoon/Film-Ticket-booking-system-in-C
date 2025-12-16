0df721547bc05647d6095246e87d78452279c7d1
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-12-16 11:42:50 +0530

    implement ticket booking functionality

---
+++ src/booking.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include "booking.h"
+#include "movie.h"
+
+void bookTicket(){
+    showMovies();
+
+    int id, seats;
+    printf("Enter movie ID to book: ");
+    scanf("%d", &id);
+
+    Movie m;
+    if(!findMovie(id, &m)){
+        printf("Movie with ID %d not found.\n", id);
+        return;
+    }
+
+    Booking b;
+    printf("Enter your name: ");
+    scanf("%s", b.customer_name);
+
+    printf("Enter your phone number: ");
+    scanf("%s", b.phone);
+
+    printf("Enter number of seats to book: ");
+    scanf("%d", &seats);
+
+    if(seats > m.available_seats){
+        printf("Only %d seats available for this movie.\n", m.available_seats);
+        return;
+    }
+
+    b.movie_id = id;
+    strcpy(b.movie_name, m.name);
+    strcpy(b.format, m.format);
+    strcpy(b.show_date, m.show_date);
+    strcpy(b.show_time, m.show_time);
+
+    b.seats = seats;
+    b.total_price = seats * m.price;
+
+    m.available_seats -= seats;
+
+    
+}
+
